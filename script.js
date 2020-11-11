c6a5820c05f186123358ad66432ad2a5df8bda48
Antonio <aantonio_94@hotmail.com>
2020-11-10 22:12:52 -0600

    changes to javascript file to enable button

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ $.ajax({
 
 
 
-$(".drink-form").on("submit",function(event){
+$(".drink-form").on("click",function(event){
   event.preventDefault();
   // Here we are building the URL we need to query the database
   var ingDrink=$("#ingredientDrink").val();
