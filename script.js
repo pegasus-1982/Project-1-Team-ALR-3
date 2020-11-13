93039dc5e9fc66d2a7a6f2942cef332d28aadb17
rakeru2006 <rakeru2006@gmail.com>
2020-11-12 21:21:36 -0600

    Adding nav menu drop to fix scream

--- script.js
+++ script.js
@@ -97,6 +97,14 @@ $(".drink-form").on("click",function(event){
 
 });
 
+//
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems, options);
+});
+// Or with jQuery
+$('.dropdown-trigger').dropdown();
+
 
 //Carrousell call inicio Raque 
 
